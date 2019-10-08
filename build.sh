2099c481acdca9113b04e2586e4421081f43c53e
Herklos <herklos.dev@protonmail.com>
2019-10-09 00:37:31 +0200

    Improved script

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 git clone $1
 cd $2
 /opt/python/$3/bin/pip install --only-binary cryptography auditwheel cryptography twine
-/opt/python/$3/bin/pisp install -r requirements.txt
 /opt/python/$3/bin/python setup.py bdist_wheel --python-tag=$4
-auditwheel repair dist/$2*.whl
-twine upload dist/$2*.whl -u $4 -p $5
+auditwheel repair $(find . -iname "*.whl")
+/opt/python/$3/bin/python -m twine upload $(find . -iname "*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD
