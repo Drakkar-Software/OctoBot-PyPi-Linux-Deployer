61f638461e0838624cc4a8c1387d5c1f4b238cae
Herklos <herklos.dev@protonmail.com>
2019-10-09 23:04:28 +0200

    Added skip existing in twine upload

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd /project
 /opt/python/$2/bin/pip install --only-binary cryptography auditwheel cryptography twine
 /opt/python/$2/bin/python setup.py bdist_wheel --python-tag=$3
 auditwheel repair /project/$(find dist -iname "*$1*.whl")
-/opt/python/$2/bin/python -m twine upload /project/$(find wheelhouse -iname "*$1*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD
+/opt/python/$2/bin/python -m twine upload /project/$(find wheelhouse -iname "*$1*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD --skip-existing
