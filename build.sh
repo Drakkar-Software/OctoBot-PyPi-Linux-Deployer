b225906ffa9c8754bb0c62293210513a40b4962c
Herklos <herklos.dev@protonmail.com>
2019-10-09 21:11:14 +0200

    Manage multiple arch in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /project
-/opt/python/$1/bin/pip install --only-binary cryptography auditwheel cryptography twine
-/opt/python/$1/bin/python setup.py bdist_wheel --python-tag=$2
-auditwheel repair /project/$(find dist -iname "*.whl")
-/opt/python/$1/bin/python -m twine upload /project/$(find wheelhouse -iname "*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD
+/opt/python/$2/bin/pip install --only-binary cryptography auditwheel cryptography twine
+/opt/python/$2/bin/python setup.py bdist_wheel --python-tag=$3
+auditwheel repair /project/$(find dist -iname "*$1*.whl")
+/opt/python/$2/bin/python -m twine upload /project/$(find wheelhouse -iname "*$1*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD
