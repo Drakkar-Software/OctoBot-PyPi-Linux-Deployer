ae0d5290e6f55df35c1c853f0c0b550f053b4755
Herklos <herklos.dev@protonmail.com>
2019-10-09 20:11:44 +0200

    Removed git clone

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-git clone $1
-cd $2
-/opt/python/$3/bin/pip install --only-binary cryptography auditwheel cryptography twine
-/opt/python/$3/bin/python setup.py bdist_wheel --python-tag=$4
-auditwheel repair $(find . -iname "*.whl")
-/opt/python/$3/bin/python -m twine upload $(find . -iname "*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD
+cd /project
+/opt/python/$1/bin/pip install --only-binary cryptography auditwheel cryptography twine
+/opt/python/$1/bin/python setup.py bdist_wheel --python-tag=$2
+auditwheel repair /project/$(find dist -iname "*.whl")
+/opt/python/$1/bin/python -m twine upload /project/$(find wheelhouse -iname "*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD
