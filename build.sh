287620652c01697fb6385731090fe831914f60a9
Herklos <herklos.dev@protonmail.com>
2019-10-15 22:54:12 +0200

    Added wheel cleanup after publish

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cd /project
 /opt/python/$2/bin/python setup.py bdist_wheel --python-tag=$3
 auditwheel repair /project/$(find dist -iname "*$1*.whl")
 /opt/python/$2/bin/python -m twine upload /project/$(find wheelhouse -iname "*$1*.whl") -u $PYPI_USERNAME -p $PYPI_PASSWORD --skip-existing
+rm -rf /project/build /project/wheelhouse /project/dist
